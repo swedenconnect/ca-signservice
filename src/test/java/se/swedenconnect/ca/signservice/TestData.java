2250909e2365606194677434a53f2129990a0a03
Stefan Santesson <stefan@aaa-sec.com>
2024-11-26 14:21:43 +0100

    Spring boot 3.4.0 and copyright

--- src/test/java/se/swedenconnect/ca/signservice/TestData.java
+++ src/test/java/se/swedenconnect/ca/signservice/TestData.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023.  Agency for Digital Government (DIGG)
+ * Copyright 2024.  Agency for Digital Government (DIGG)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
